3f9c3f19377b3707ef2f5ec0814f4727ee2a5fd3
LukasW <48596358+Luprogrammierung@users.noreply.github.com>
2021-09-28 14:34:40 +0200

    second-page responsive

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const firstpage = document.getElementById('first-page');
-const elementsleft = document.getElementsByClassName('container-left');
-const elementsright = document.getElementsByClassName('container-right');
 const timeline = document.getElementById('second-page-Timeline');
 const contentbox = document.getElementById('second-page-Contentbox');
+const elementsleft = document.getElementsByClassName('container-left');
+const elementsright = document.getElementsByClassName('container-right');
+const elementscenter = document.getElementsByClassName('container-center');
+const elementsall = document.getElementsByClassName('container');
 
 function gitlinkedadd() {
     if (firstpage.clientHeight * 4 > firstpage.clientWidth * 7) {
@@ -27,36 +29,65 @@ function removeNewsbar() {
     }
 }
 function positionelements() {
-    var heightleft = 0;
-    var heightright = elementsright[0].offsetTop;
-    console.log(elementsright[0].style.top);
-    for (var i = 0; i < elementsleft.length; i++) {
-        elementsleft[i].style.top = heightleft + "px";
-        console.log(parseInt(getComputedStyle(elementsleft[i]).marginBottom) + elementsleft[i].offsetHeight)
-        heightleft = heightleft + parseInt(getComputedStyle(elementsleft[i]).marginBottom) + elementsleft[i].offsetHeight;
+    if (elementsleft.length > 1) {
+        var heightleft = 0;
+        var heightright = 70;
+        for (var i = 0; i < elementsleft.length; i++) {
+            elementsleft[i].style.top = heightleft + "px";
+            heightleft = heightleft + parseInt(getComputedStyle(elementsleft[i]).marginBottom) + elementsleft[i].offsetHeight;
+        }
+        for (var i = 0; i < elementsright.length; i++) {
+            elementsright[i].style.top = heightright + "px";
+            heightright = heightright + parseInt(getComputedStyle(elementsright[i]).marginBottom) + elementsright[i].offsetHeight;
+        }
+        if (heightright > heightleft) {
+            timeline.style.height = heightright + "px";
+        }
+        else {
+            timeline.style.height = heightleft + "px"
+        }
     }
-    for (var i = 0; i < elementsright.length; i++) {
-        elementsright[i].style.top = heightright + "px";
-        console.log(parseInt(heightright));
-        heightright = heightright + parseInt(getComputedStyle(elementsright[i]).marginBottom) + elementsright[i].offsetHeight;
+    else {
+        heightcenter = 0;
+        for (var i = 0; i < elementscenter.length; i++) {
+            elementscenter[i].style.top = heightcenter + "px";
+            heightcenter = heightcenter + parseInt(getComputedStyle(elementscenter[i]).marginBottom) + elementscenter[i].offsetHeight;
+        }
+        timeline.style.height = heightcenter + "px"
     }
-    if(heightright>heightleft){
-        timeline.style.height = heightright + "px";
+    contentbox.style.height = timeline.offsetHeight + timeline.offsetTop + "px";
+
+}
+function mobiledesign() {
+    if (window.innerWidth < 700) {
+        for (var i = 0; i < elementsall.length; i++) {
+            elementsall[i].classList.add('container-center');
+            elementsall[i].classList.remove('container-left');
+            elementsall[i].classList.remove('container-right');
+        }
     }
     else{
-        timeline.style.height = heightleft + "px"
+        for(var i=1;i<elementsall.length+1; i++){
+            elementsall[i-1].classList.remove('container-center');
+            if(i%2 ==0){
+                elementsall[i-1].classList.add('container-right');
+            }
+            else{
+                elementsall[i-1].classList.add('container-left');
+            }
+        }
     }
-    console.log(timeline.offsetHeight + timeline.offsetTop);
-    contentbox.style.height = timeline.offsetHeight + timeline.offsetTop + "px"  ;
-
 }
 
 window.addEventListener('load', () => {
+    mobiledesign();
     positionelements();
     gitlinkedadd();
     removeNewsbar();
 });
 window.addEventListener('resize', () => {
+    console.log(elementsright);
+    mobiledesign();
     positionelements();
     gitlinkedadd();
     removeNewsbar();
